d0206486cdc6f330077b66b5b19cd8ea175cdb2b
Vanessa Thamiris <148474358+van-g206@users.noreply.github.com>
2026-01-02 12:25:38 -0300

    atualização

--- sw.js
+++ sw.js
@@ -4,16 +4,19 @@ const assets = [
   './index.html',
   './formata.css',
   './scripts.js',
+  './site.webmanifest',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
+// Instala o Service Worker e guarda os arquivos no cache
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
+// Responde as requisições usando o cache quando offline
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
